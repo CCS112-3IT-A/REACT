94a30f71c9ca2ce7ef414d9814b5a808e6a4f1b3
Jhenacris Dela Cruz <delacruzjhenacris34@gmail.com>
2024-04-17 13:59:12 +0800

    Add To Cart

---
+++ src/components/AddToCart.js
@@ -0,0 +1,14 @@
+// AddToCartButton.js
+import React from 'react';
+
+function AddToCart({ product, addToCart }) {
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
+  return (
+    <button onClick={handleAddToCart}>Add to Cart</button>
+  );
+}
+
+export default AddToCart;
