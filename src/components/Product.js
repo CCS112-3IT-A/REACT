f872bcc117bb9d06675c8ad5f0b18192ae85bdc1
andreiCCenteno <160735547+andreiCCenteno@users.noreply.github.com>
2024-04-17 12:00:11 +0800

    Add files via upload
    content of product.js

---
+++ src/components/Product.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Products from './ProductInformation';
+import AddToCart from './AddToCart';
+
+function Product({ addToCart }) {
+  const [products] = useState([
+    { name: 'Electric Toothbrush', id: 1, price: 1000, description: 'Motorized toothbrush'},
+    { name: 'Apple Watch', id: 2, price: 50000, description: 'Smartwatch'},
+    { name: 'Airpods', id: 3, price: 10000, description: 'Wireless earbuds'},
+    { name: 'Mobile Phone', id: 4, price: 8500, description: 'Smartphone'},
+    { name: 'Digicam', id: 5, price: 20000, description: 'Digital camera'},
+    { name: 'Cellphone Case', id: 6, price: 500, description: 'Protective case'},
+    { name: 'Wireless Mouse', id: 7, price: 1500, description: 'Computer mouse'},
+    { name: 'Mechanical Keyboard', id: 8, price: 15000, description: 'Gaming keyboard'},
+    { name: 'Instax', id: 9, price: 4000, description: 'Instant camera'},
+    { name: 'Printer', id: 10, price: 5000, description: 'Printer'}
+  ]);
+
+  return(
+    <div className="product-information">
+      {products.map((product, index) => (
+        <div key={product.id} className="product">
+          <Products
+            key={index}
+            number={index + 1}
+            name={product.name}
+            price={product.price}
+            id={product.id}
+            description={product.description}
+          />
+          <AddToCart product={product} addToCart={addToCart} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Product;
